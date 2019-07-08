649ad83df7a668bfef99a664b352f0da5321ab06
Steve Callear <scallear@gmail.com>
2019-07-08 19:43:52 +0100

    Add mod support (#2)
    * Add go.mod
    
    * Update license
    
    * Update go versions
    
    * Add whitespace

--- roz.go
+++ roz.go
@@ -8,24 +8,26 @@ import (
 )
 
 var (
-	characters = []*unicode.RangeTable{unicode.Letter, unicode.Number}
-	separators = []*unicode.RangeTable{unicode.White_Space, unicode.Dash}
+	chars = []*unicode.RangeTable{unicode.Letter, unicode.Number}
+	seps  = []*unicode.RangeTable{unicode.White_Space, unicode.Dash}
 )
 
 // New returns a new slug for the specified strings
-func New(vals ...string) string {
-	str := norm.NFKD.String(strings.Join(vals, "-"))
+func New(values ...string) string {
+	str := norm.NFKD.String(strings.Join(values, "-"))
 	buf := make([]rune, 0, len(str))
+
 	sep := false
 	for _, r := range str {
 		switch {
-		case unicode.IsOneOf(characters, r):
+		case unicode.IsOneOf(chars, r):
 			buf = append(buf, unicode.ToLower(r))
 			sep = false
-		case !sep && unicode.IsOneOf(separators, r):
+		case !sep && unicode.IsOneOf(seps, r):
 			buf = append(buf, '-')
 			sep = true
 		}
 	}
+
 	return strings.Trim(string(buf), "-")
 }
--- roz_test.go
+++ roz_test.go
@@ -15,49 +15,51 @@ func ExampleNew() {
 
 func TestNew(t *testing.T) {
 	tests := []struct {
-		name string
-		vals []string
-		exp  string
+		name   string
+		values []string
+		exp    string
 	}{
 		{
-			name: "should return empty for empty values",
-			vals: []string{},
-			exp:  "",
+			name:   "should return empty for empty values",
+			values: []string{},
+			exp:    "",
 		},
 		{
-			name: "should not modify a valid slug",
-			vals: []string{"a1-b2-c3"},
-			exp:  "a1-b2-c3",
+			name:   "should not modify a valid slug",
+			values: []string{"a1-b2-c3"},
+			exp:    "a1-b2-c3",
 		},
 		{
-			name: "should remove duplicate dashes",
-			vals: []string{"--a1---b2-"},
-			exp:  "a1-b2",
+			name:   "should remove duplicate dashes",
+			values: []string{"--a1---b2-"},
+			exp:    "a1-b2",
 		},
 		{
-			name: "should convert to lower case",
-			vals: []string{"A1 B2"},
-			exp:  "a1-b2",
+			name:   "should convert to lower case",
+			values: []string{"A1 B2"},
+			exp:    "a1-b2",
 		},
 		{
-			name: "should simplify special characters",
-			vals: []string{"Şpècïal Ĉhãractêrs ✓"},
-			exp:  "special-characters",
+			name:   "should simplify special characters",
+			values: []string{"Şpècïal Ĉhãractêrs ✓"},
+			exp:    "special-characters",
 		},
 		{
-			name: "should remove white space",
-			vals: []string{" a1\nb2\t"},
-			exp:  "a1-b2",
+			name:   "should remove white space",
+			values: []string{" a1\nb2\t"},
+			exp:    "a1-b2",
 		},
 		{
-			name: "should combine multiple values",
-			vals: []string{"  ", " a1\t", "--", "\nb2  "},
-			exp:  "a1-b2",
+			name:   "should combine multiple values",
+			values: []string{"  ", " a1\t", "--", "\nb2  "},
+			exp:    "a1-b2",
 		},
 	}
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(st *testing.T) {
-			act := roz.New(tt.vals...)
+			act := roz.New(tt.values...)
+
 			if act != tt.exp {
 				t.Errorf("got %s, expected %s", act, tt.exp)
 			}
